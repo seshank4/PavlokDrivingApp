b9939405f2f4ff05df7b6e2b0e98a36b6a9f5d00
DESKTOP-M2P287P\sesha <seshank4@gmail.com>
2016-12-05 01:53:11 -0500

    Back Button redirect fix

--- PavlokDrivingApp/app/src/main/java/edu/bu/cs591/ateam/pavlokdrivingapp/MainActivity.java
+++ PavlokDrivingApp/app/src/main/java/edu/bu/cs591/ateam/pavlokdrivingapp/MainActivity.java
@@ -373,6 +373,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (mDrawerToggle.onOptionsItemSelected(item)) {
             return true;
